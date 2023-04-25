4e201a6f694b4a6e281482af185af4ef694aee1f
YoshiYK8 <yoshiyk8@gmail.com>
2023-04-24 20:11:12 -0500

    ADD: Variables.

---
+++ variables.py
@@ -0,0 +1,7 @@
+
+name = "Yoshio"
+age = 21
+list_shopping = "milk", "Eggs", "Vegetables"
+
+print(f"{name}, {age}") #f = literal templeates, ahorras el proceso de print(name + " " + age)
+print(list_shopping)
