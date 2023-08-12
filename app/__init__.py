becd35c1d4e307f753832c663b407706c6313736
Yoshio Lomas <yoshiyk8@gmail.com>
2023-08-11 21:05:06 -0600

    hotfix/ErrorDeImports

--- app/__init__.py
+++ app/__init__.py
@@ -1,17 +1,24 @@
 from flask import Flask
 from flask.views import MethodView
 from .Users.resources import users_blueprint
+from .Database import db
 
 class HelloWorld(MethodView):
     def get(self):
-        return {'message': "Hey there! Hello World!"}
-    
+        return {'message': "Prueba debug flask"}
+    #def post(self):
+        #Subir los cambios a la nube
+    #def patch(self):
+        #Reemplazar con cambios especificos en la nube
+    #def delete(self):
+        #Borrar cosas de la nube con datos especificos
+
 def create_app():
     app = Flask(__name__)
-
+    app.config.from_pyfile('../settings.py')
     hello_world = HelloWorld.as_view("hello_world")
     app.add_url_rule('/', view_func=hello_world)
     app.add_url_rule('/api/', view_func = hello_world)
-    app.add_url_rule('/new_url/', view_func = hello_world)
     app.register_blueprint(users_blueprint)
+
     return app  
